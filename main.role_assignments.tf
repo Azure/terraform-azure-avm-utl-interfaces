a8a43351cc1b22aa45a703853639c8069e9d6d7b
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-02-19 10:39:31 +0000

    fix: condition

--- main.role_assignments.tf
+++ main.role_assignments.tf
@@ -1,7 +1,7 @@
 # This resource allows us to look up role definitions by role name.
 # The AzureRM provider does this already so we are replicating this functionality here to benefit AzAPI users.
 data "azapi_resource_list" "role_definitions" {
-  count = var.role_assignment_definition_lookup_enabled && length(var.role_assignment_definition_scope) > 0 ? 1 : 0
+  count = var.role_assignment_definition_lookup_enabled && length(var.role_assignments) > 0 ? 1 : 0
 
   parent_id = var.role_assignment_definition_scope == null ? "/" : var.role_assignment_definition_scope
   type      = "Microsoft.Authorization/roleDefinitions@2022-04-01"
