9113e8681d5f7ed426e97bae6e7f2f488ad5a58f
Jon Chancellor <jchancellor@microsoft.com>
2025-06-30 16:07:42 -0700

    pe null reference

--- terraform.tf
+++ terraform.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "~> 1.9"
   required_providers {
     azapi = {
-      source  = "azure/azapi"
-      version = "~> 2.0"
+      source  = "Azure/azapi"
+      version = "~> 2.4"
     }
     modtm = {
       source  = "azure/modtm"
