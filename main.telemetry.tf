6afc30f3e3e98a48c1aed05c19486e8f7b6931a1
Jon Chancellor <jchancellor@microsoft.com>
2025-07-07 14:46:02 -0700

    spaces hack

--- main.telemetry.tf
+++ main.telemetry.tf
@@ -22,9 +22,6 @@ resource "modtm_telemetry" "telemetry" {
     module_version  = one(data.modtm_module_source.telemetry).module_version
     random_id       = one(random_uuid.telemetry).result
   }, { location = local.main_location })
-
-
-
 }
 locals {
   main_location = "unknown"
@@ -40,9 +37,6 @@ locals {
     avm_module_source  = one(data.modtm_module_source.telemetry).module_source
     avm_module_version = one(data.modtm_module_source.telemetry).module_version
   })
-
-
-
 }
 
 locals {
@@ -56,9 +50,6 @@ locals {
     "git::https://github\\.com/[A|a]zure/.+",
     "git::ssh:://git@github\\.com/[A|a]zure/.+",
   ]
-
-
-
 }
 
 locals {
